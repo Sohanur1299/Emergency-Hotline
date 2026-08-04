81488a3dc0f2f76d8b42769896480b50d3f2ff46
Sohanur Rahman Khan <166442871+Sohanur1299@users.noreply.github.com>
2026-08-04 18:52:07 +0600

    coin funcionality and servicename,servicenumber showing feature updated

--- script.js
+++ script.js
@@ -21,28 +21,36 @@ document.getElementById("card-section").addEventListener('click', ((event) => {
         currentHeartCount.innerText = getHeartCount;
     }
 
-    // copy-btn-class
-    // if (event.target.classList.contains("copy-btn-class")) {
-    //     console.log('this is copy button class!');
-    //     console.log(event.target.classList.value);
-    // }
-    // else{
-    //     console.log('not a copy button cclass');
-    // }
+
     const btn = event.target.closest(".copy-btn-class"); // starting from where I clicked, upward asking does it have copy btn class? no? go upward
     if (btn) {
-        // console.log(btn.innerText);
-        // console.log(btn.parentNode.parentNode);
-        // nthCard = btn.parentNode.parentNode; // whole card 
-        nthCard = btn.closest(".nth-card"); // whole nth card
-        // console.log(nthCard);
-        // element.querySelector(selector) - searches inside the element only
-        serviceName = nthCard.querySelector(".service-name");
-        serviceNumber = nthCard.querySelector(".service-number");
-        console.log(serviceName.innerText, serviceNumber.innerText);
-        
-        
-        
+        const getCoinsElement = document.getElementById("show-coin");
+        const availableCoins = parseInt(getCoinsElement.innerText);
+        if (availableCoins >= 20) {
+            // console.log(btn.innerText);
+            // console.log(btn.parentNode.parentNode);
+            // nthCard = btn.parentNode.parentNode; // whole card 
+            nthCard = btn.closest(".nth-card"); // whole nth card
+            // console.log(nthCard);
+            // element.querySelector(selector) - searches inside the element only
+            serviceName = nthCard.querySelector(".service-name");
+            serviceNumber = nthCard.querySelector(".service-number");
+            console.log(serviceName.innerText, serviceNumber.innerText);
+            alert(`Service Name: ${serviceName.innerText}\nService Number:${serviceNumber.innerText}`);
+
+            // update coins
+            newCoins = availableCoins - 20;
+            getCoinsElement.innerText = newCoins;
+
+            // store into call history
+        }
+        else {
+            alert("You do not enough coins.")
+            return;
+        }
+
+
+
     } else {
         console.log('not the COPY button!');
     }
