21a8db765c7b0fed0b750c02f6088f418b00e970
Sohanur Rahman Khan <166442871+Sohanur1299@users.noreply.github.com>
2026-08-04 17:48:27 +0600

    heart functionality developed

--- script.js
+++ script.js
@@ -7,5 +7,20 @@ coinViewer.innerText = 100; // default coin
 const copyCount = document.getElementById("copy-count");
 copyCount.innerText = 0; // default copy
 
+const heartCount = document.getElementById("heart-count");
+heartCount.innerText = 0; // default heartcount
+
+document.getElementById("card-section").addEventListener('click', ((event)=>{
+    // console.log(event.target.classList.value);
+    if(event.target.classList.contains("class-heart")){
+        console.log("found");
+        const currentHeartCount = document.getElementById("heart-count"); // current heart count
+        let getHeartCount = parseInt(currentHeartCount.innerText);
+        getHeartCount += 1;
+        currentHeartCount.innerText = getHeartCount;
+    }else{
+        console.log("There is some issues!");
+    }
+}))
 
 
