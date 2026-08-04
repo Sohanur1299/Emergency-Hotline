3b55bb83c9abea3c0fdf998ea22d19ad08f4c0f1
Sohanur Rahman Khan <166442871+Sohanur1299@users.noreply.github.com>
2026-08-04 18:40:13 +0600

    half of call functionality:

--- script.js
+++ script.js
@@ -10,16 +10,41 @@ copyCount.innerText = 0; // default copy
 const heartCount = document.getElementById("heart-count");
 heartCount.innerText = 0; // default heartcount
 
-document.getElementById("card-section").addEventListener('click', ((event)=>{
+// Implemented using event delegation technique
+document.getElementById("card-section").addEventListener('click', ((event) => {
     // console.log(event.target.classList.value);
-    if(event.target.classList.contains("class-heart")){
-        console.log("found");
+    if (event.target.classList.contains("class-heart")) {
+        // console.log("found");
         const currentHeartCount = document.getElementById("heart-count"); // current heart count
         let getHeartCount = parseInt(currentHeartCount.innerText);
         getHeartCount += 1;
         currentHeartCount.innerText = getHeartCount;
-    }else{
-        console.log("There is some issues!");
+    }
+
+    // copy-btn-class
+    // if (event.target.classList.contains("copy-btn-class")) {
+    //     console.log('this is copy button class!');
+    //     console.log(event.target.classList.value);
+    // }
+    // else{
+    //     console.log('not a copy button cclass');
+    // }
+    const btn = event.target.closest(".copy-btn-class"); // starting from where I clicked, upward asking does it have copy btn class? no? go upward
+    if (btn) {
+        // console.log(btn.innerText);
+        // console.log(btn.parentNode.parentNode);
+        // nthCard = btn.parentNode.parentNode; // whole card 
+        nthCard = btn.closest(".nth-card"); // whole nth card
+        // console.log(nthCard);
+        // element.querySelector(selector) - searches inside the element only
+        serviceName = nthCard.querySelector(".service-name");
+        serviceNumber = nthCard.querySelector(".service-number");
+        console.log(serviceName.innerText, serviceNumber.innerText);
+        
+        
+        
+    } else {
+        console.log('not the COPY button!');
     }
 }))
 
