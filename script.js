e600f94edeefeeb2d0f73adb4ca60d2e33dd50c1
Sohanur Rahman Khan <166442871+Sohanur1299@users.noreply.github.com>
2026-08-04 12:37:52 +0600

    copy count, coins tracked

---
+++ script.js
@@ -0,0 +1,11 @@
+
+// coin setting
+const coinViewer = document.getElementById("show-coin");
+coinViewer.innerText = 100; // default coin
+
+// Copy Count
+const copyCount = document.getElementById("copy-count");
+copyCount.innerText = 0; // default copy
+
+
+
